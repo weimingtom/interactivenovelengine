8d86f2cb76996e17b11ddb665d480185bc772ee2
shepelt@gmail.com <shepelt@gmail.com@cbfd3312-cf91-11de-bb02-e302c5db49d5>
2010-08-26 14:14:45 +0000

    [engine] -added new component Label -added sampler test game

---
+++ trunk/inegine/src/engine/Effector/Graphics/Components/Label/Label.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SlimDX.Direct3D9;
+
+namespace INovelEngine.Effector
+{
+    class Label : Button
+    {
+        protected override void DrawInternal()
+        {
+            sprite.Begin(SpriteFlags.AlphaBlend);
+
+            DrawText();
+
+            sprite.End();
+        }
+    }
+}
