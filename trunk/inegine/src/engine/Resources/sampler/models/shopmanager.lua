0ebad126803bce54df4f7568d8c69847d40c28c2
shepelt <shepelt@cbfd3312-cf91-11de-bb02-e302c5db49d5>
2011-01-28 16:19:02 +0000

    - changed csv manager to accept string column ids

--- trunk/inegine/src/engine/Resources/sampler/models/shopmanager.lua
+++ trunk/inegine/src/engine/Resources/sampler/models/shopmanager.lua
@@ -13,28 +13,7 @@ end
 
 function ShopManager:Load()
     self.csv = GetCsv("shopdata");
-    for i=0, self.csv.ColumnCount-1 do
-        local c = self.csv:GetColumn(i);
-        if c == "category" then self.categoryIndex = i;
-        elseif c == "id" then self.idIndex = i;
-        else 
-            Trace(c .. " does not match anything");
-        end
-    end
-
     self.shopcsv = GetCsv("shoplist");
-    for i=0, self.shopcsv.ColumnCount-1 do
-        local c = self.shopcsv:GetColumn(i);
-        if c == "id" then self.shopidIndex = i;
-        elseif c == "name" then self.shopnameIndex = i;
-        elseif c == "owner" then self.shopownerIndex = i;
-        elseif c == "greetings" then self.shopgreetingsIndex = i;
-        elseif c == "portrait" then self.shopportraitIndex = i;
-        elseif c == "buymessage" then self.shopbuymessageIndex = i;
-        else 
-            Trace(c .. " does not match anything");
-        end
-    end
 end
 
 function ShopManager:ItemExists(id)
@@ -43,15 +22,15 @@ end
 
 function ShopManager:ExtractItem(i)
     local item = {};
-    item.category = self.csv:GetString(i, self.categoryIndex);
-    item.id = self.csv:GetString(i, self.idIndex);
+    item.category = self.csv:GetString(i, "category");
+    item.id = self.csv:GetString(i, "id");
     return item;
 end
 
 function ShopManager:GetItems(category)
 	local itemList = {};
     for i=0, self.csv.Count-1 do
-    local itemCategory = self.csv:GetString(i, self.categoryIndex);
+    local itemCategory = self.csv:GetString(i, "category");
         if (category == itemCategory) then
 			table.insert(itemList, self:ExtractItem(i));
         end
@@ -62,7 +41,7 @@ end
 function ShopManager:GetItemCount(category)
 	local count = 0;
 	for i=0, self.csv.Count-1 do
-        local c = self.csv:GetString(i, self.categoryIndex);
+        local c = self.csv:GetString(i, "category");
         if (category == c) then count = count + 1;
         end
     end
@@ -71,19 +50,18 @@ end
 
 function ShopManager:ExtractShop(i)
     local item = {};
-    item.id = self.shopcsv:GetString(i, self.shopidIndex);
-    item.name = self.shopcsv:GetString(i, self.shopnameIndex);
-    item.owner = self.shopcsv:GetString(i, self.shopownerIndex);
-    item.greetings = self.shopcsv:GetString(i, self.shopgreetingsIndex);
-    item.portrait = self.shopcsv:GetString(i, self.shopportraitIndex);
-    item.buymessage = self.shopcsv:GetString(i, self.shopbuymessageIndex);
+    item.id = self.shopcsv:GetString(i, "id");
+    item.name = self.shopcsv:GetString(i, "name");
+    item.owner = self.shopcsv:GetString(i, "owner");
+    item.greetings = self.shopcsv:GetString(i, "greetings");
+    item.portrait = self.shopcsv:GetString(i, "portrait");
+    item.buymessage = self.shopcsv:GetString(i, "buymessage");
     return item;
 end
 
 function ShopManager:GetShop(shop)
     for i=0, self.shopcsv.Count-1 do
-    local shopId = self.shopcsv:GetString(i, self.shopidIndex);
-        Trace(shop .. " vs " .. shopId);
+    local shopId = self.shopcsv:GetString(i, "id");
         if (shop == shopId) then
 			return self:ExtractShop(i);
         end
