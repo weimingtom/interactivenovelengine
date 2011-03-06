e2405ea9bbcd7e1ea0e0a5c3a9c87d6d01f617c8
shepelt <shepelt@cbfd3312-cf91-11de-bb02-e302c5db49d5>
2011-03-06 08:44:31 +0000

    -implemented character & status view

--- trunk/inegine/src/engine/Resources/test/characterTest.lua
+++ trunk/inegine/src/engine/Resources/test/characterTest.lua
@@ -6,31 +6,27 @@ TestCharacter = {}
     function TestCharacter:setUp()
 		self.character = Character:New();
 		local character = self.character;
-		character:SetFirstName("¾È³ª");
-		character:SetLastName("±è");
-		character:Set("age", 12);
-		character:Set("gold", 1000);
-		character:Set("stress", 50);
-		character:Set("mana", 100);
     end
 
 
     function TestCharacter:testGetSetStatus()
 		local character = self.character;
-		character:Set("con", 1);
-		character:Set("int", 2);
-		character:Set("cha", 3);
-		assertEquals(1, character:Get("con"))
-		assertEquals(2, character:Get("int"))
-		assertEquals(3, character:Get("cha"))
+		character:Set("int", 1);
+		character:Set("cha", 2);
+		character:Set("wis", 3);
+		assertEquals(1, character:Get("int"))
+		assertEquals(2, character:Get("cha"))
+		assertEquals(3, character:Get("wis"))
     end
 
     function TestCharacter:testSave()
 		local character = self.character;
-		character:Set("con", 1);
-		character:Set("int", 2);
-		character:Set("cha", 3);
+		character:Set("int", 1);
+		character:Set("cha", 2);
+		character:Set("wis", 3);
 
         local saveString = character:Save("character");
         print(saveString);
+        
+		assert(loadstring(saveString))();
     end
